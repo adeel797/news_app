a62ee61d1a46246f24936304e52846292f4ea87e
Adeel Ahmed <141135530+adeel797@users.noreply.github.com>
2025-08-06 15:23:41 +0500

    constants.dart

--- lib/ui/core/constants.dart
+++ lib/ui/core/constants.dart
@@ -1,6 +1,7 @@
 class Constants {
-  static const String newsApiKey = "a888414db19241ceb74beaae282cee9e";
+  static const String newsApiKey = "YOUR API KEY HERE";
   static const String newsApiBaseUrl = "https://newsapi.org/v2";
   static const String placeholderImage =
       "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
 }
+
